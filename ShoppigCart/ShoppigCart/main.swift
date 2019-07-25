155140f56ad59813a6608594a3d4dc12b1216156
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-07-25 10:24:49 -0400

    shoppingCart-Project

---
+++ ShoppigCart/ShoppigCart/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ShoppigCart
+//
+//  Created by Mr Wonderful on 7/25/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
