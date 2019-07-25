7eec021388154879d5b08eddde82c07d7fa10195
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-07-25 10:23:37 -0400

    check

---
+++ Assesment1/Assesment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assesment1
+//
+//  Created by albert coelho oliveira on 7/25/19.
+//  Copyright © 2019 albert coelho oliveira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
